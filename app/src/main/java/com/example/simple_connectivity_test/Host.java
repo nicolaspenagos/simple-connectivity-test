d2c58c53aa37412750ab8cbf320e6114681e39e8
Nicolas Penagos Montoya <nicolas.penagosm98@gmail.com>
2020-09-08 00:22:28 -0500

    Main activity gui and class' skeleton have been coded. Ping and Host empty activities have been created.

---
+++ app/src/main/java/com/example/simple_connectivity_test/Host.java
@@ -0,0 +1,14 @@
+package com.example.simple_connectivity_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Host extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_host);
+    }
+}
--- app/src/main/java/com/example/simple_connectivity_test/MainActivity.java
+++ app/src/main/java/com/example/simple_connectivity_test/MainActivity.java
@@ -1,14 +1,43 @@
+/**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * @author Nicolás Penagos Montoya
+ * nicolas.penagosm98@gmail.com
+ **~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
 package com.example.simple_connectivity_test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    // -------------------------------------
+    // XML references
+    // -------------------------------------
+    private EditText ping1EditText;
+    private EditText ping2EditText;
+    private EditText ping3EditText;
+    private EditText ping4EditText;
+    private Button pingButton;
+    private Button searchHostButton;
+
+    // -------------------------------------
+    // Gui methods
+    // -------------------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ping1EditText = findViewById(R.id.ping1EditText);
+        ping2EditText = findViewById(R.id.ping2EditText);
+        ping3EditText = findViewById(R.id.ping3EditText);
+        ping4EditText = findViewById(R.id.ping4EditText);
+        pingButton = findViewById(R.id.pingButton);
+        searchHostButton = findViewById(R.id.searchHostButton);
+
     }
 }
